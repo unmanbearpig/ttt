226e9343cde4b4d945cc9fe6d55aa17dd27a8667
unmanbearpig <unmanbearpig@gmail.com>
2017-03-14 12:58:30 +0300

    Add running

--- src/Command.hs
+++ src/Command.hs
@@ -9,6 +9,10 @@ parse 'f' = Move $ Coordinates 0 1
 parse 'b' = Move $ Coordinates 0 (-1)
 parse 'n' = Move $ Coordinates 1 0
 parse 'p' = Move $ Coordinates (-1) 0
+parse 'F' = Move $ Coordinates 0 5
+parse 'B' = Move $ Coordinates 0 (-5)
+parse 'N' = Move $ Coordinates 5 0
+parse 'P' = Move $ Coordinates (-5) 0
 parse _   = NoOp
 
 getCommand :: IO Command
